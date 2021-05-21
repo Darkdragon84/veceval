4ccd17b8a0d8228ea427a62eb681ec045499cac0
Valentin <valentin@iris.ai>
2021-05-21 12:53:18 +0200

    - minor edits to data preparation for NLI

--- data/nli/scripts/prepare_data.sh
+++ data/nli/scripts/prepare_data.sh
@@ -3,5 +3,8 @@ tar -zxf ppdb-2.0-human-labels.tgz
 mv human-labeled-data/ppdb-sample.tsv ./
 rm -r human-labeled-data/
 rm ppdb-2.0-human-labels.tgz
+rm ._human-labeled-data
 
 python make_datasets.py ./ppdb-sample.tsv ../
+
+rm ppdb-sample.tsv
