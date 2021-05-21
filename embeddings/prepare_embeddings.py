801171af8673335248abb371d6db2f2a9f53bd68
Valentin <valentin@iris.ai>
2021-05-21 13:31:45 +0200

    - released vector dimension

--- embeddings/prepare_embeddings.py
+++ embeddings/prepare_embeddings.py
@@ -1,10 +1,10 @@
 import pickle
-import veceval as ve
+import training.veceval as ve
 import sys
 import gzip
 import numpy as np
 
-DIM = 50
+# DIM = 50
 SEP = "\t"
 
 
@@ -17,11 +17,13 @@ def main():
 
     embedding_dict = {}
     unk_vectors = []
+    dim = None
 
     with gzip.open(embeddings_file, 'r') as embedding_file:
         for line in embedding_file:
             this_line = line.decode().split(SEP)
-            assert len(this_line) == DIM + 1  # does this mean we can only use dim = 50?
+            dim = dim or len(this_line) - 1
+            assert len(this_line) == dim + 1
             word = this_line[0].lower()
             vector = np.array([float(x) for x in this_line[1:]])
             if word in common_vocabulary:
@@ -29,11 +31,11 @@ def main():
             else:
                 unk_vectors.append(vector)
 
-    embedding_dict[ve.PAD] = np.zeros(shape=(DIM,))
+    embedding_dict[ve.PAD] = np.zeros(shape=(dim,))
     if unk_vectors:
         embedding_dict[ve.UNK] = sum(unk_vectors) / len(unk_vectors)
     else:
-        embedding_dict[ve.UNK] = np.zeros(shape=(DIM,))
+        embedding_dict[ve.UNK] = np.zeros(shape=(dim,))
 
     with open(output_file, 'wb') as output_file:
         pickle.dump(embedding_dict, output_file)
