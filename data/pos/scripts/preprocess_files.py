ea2346d4a66d939a9d9892005c250f18e529edee
Valentin <valentin@iris.ai>
2021-05-21 12:23:45 +0200

    - fixed data preparation for POS tagging using nltk - fixed POS training

--- data/pos/scripts/preprocess_files.py
+++ data/pos/scripts/preprocess_files.py
@@ -1,25 +1,32 @@
 import sys
+from nltk.corpus import treebank
+
+NONE_TAG = "-NONE-"
+
 
 def make_pos_map(path):
     pos_map = {}
-    for line in open(path,'r'):
+    for line in open(path, 'r'):
         (tag, u_tag) = line.split()
         if '|' not in tag:
             pos_map[tag] = u_tag
     return pos_map
 
-def main():
-  POS_map = make_pos_map(sys.argv[1])
-  
-  for out_name, in_name in zip(["train.txt", "dev.txt"],
-                               ["train-wsj-0-18", "test-wsj-19-21"]):
-    with open(in_name, 'r') as in_file:
-      with open(out_name, 'w') as out_file:
-        for line in in_file:
-          for token in line.split():
-            (word, tag) = token.rsplit("_",1)
-            out_file.write(word.lower()+"\t"+POS_map[tag]+"\n")
-        out_file.write("\n")
 
-if __name__=="__main__":
+def main():
+    POS_map = make_pos_map(sys.argv[1])
+
+    train_fileids = treebank.fileids()[:18]
+    dev_fileids = treebank.fileids()[18:21]
+    for out_name, in_name in zip(["train.txt", "dev.txt"],
+                                 [train_fileids, dev_fileids]):
+        with open(out_name, 'w') as out_file:
+            for word, tag in treebank.tagged_words(in_name):
+                if tag == NONE_TAG:
+                    continue
+                out_file.write(word.lower() + "\t" + POS_map[tag] + "\n")
+            out_file.write("\n")
+
+
+if __name__ == "__main__":
     main()
