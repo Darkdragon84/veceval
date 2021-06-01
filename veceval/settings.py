32d088d520f98408d76243724c23658473bcdaf1
Valentin <valentin@iris.ai>
2021-06-01 17:50:29 +0200

    TASK [Integration of chemistry NER] https://app.asana.com/0/480028587563467/1200403399880773  - modified veceval_evaluate.py and settings.py to choose task - dataset pairs individually

--- veceval/settings.py
+++ veceval/settings.py
@@ -2,20 +2,28 @@ import os
 
 from pathlib import Path
 
-AVAILABLE_TASKS = ["chunk", "ner", "nli", "pos", "questions", "sentiment"]
-AVAILABLE_MODES = ["fixed", "finetuned"]
-AVAILABLE_DATASETS = {
-    "ner": ["conll", "chemdner"]
-}
 
-TASKS = AVAILABLE_TASKS
-MODES = AVAILABLE_MODES
-TASK_TO_DATASET = {task: datasets[0] for task, datasets in AVAILABLE_DATASETS.items()}
+def valid_path(path):
+    return os.path.isdir(path.as_posix()) and "_" not in path.stem
 
-AFFILIATION = "iris.ai"
 
 PROJECT_ROOT = Path(__file__).resolve().parent
 CONFIGS_FOLDER = PROJECT_ROOT / Path("training") / Path("configs")
+DATA_FOLDER = PROJECT_ROOT / Path("data")
+
+AVAILABLE_MODES = ["fixed", "finetuned"]
+AVAILABLE_TASK_TO_DATASET = {
+    task: [
+        dataset for dataset in os.listdir(DATA_FOLDER / task) if valid_path(DATA_FOLDER / task / dataset)
+    ]
+    for task in os.listdir(DATA_FOLDER) if valid_path(DATA_FOLDER / task)
+}
+
+MODES = AVAILABLE_MODES
+TASK_TO_DATASET = AVAILABLE_TASK_TO_DATASET
+
+AFFILIATION = "iris.ai"
+
 
 # create settings_local.py file and override the desired settings
 try:
@@ -34,12 +42,10 @@ try:
 except ImportError:
     print('No settings_local.py, using settings values from settings.py')
 
-TASKS = [task for task in TASKS if task in AVAILABLE_TASKS]
 MODES = [mode for mode in MODES if mode in AVAILABLE_MODES]
-
 TASK_TO_DATASET = {
-    task: dataset if dataset in AVAILABLE_DATASETS[task] else AVAILABLE_DATASETS[task][0]
-    for task, dataset in TASK_TO_DATASET.items() if task in AVAILABLE_TASKS
+    task: [dataset for dataset in datasets if dataset in AVAILABLE_TASK_TO_DATASET[task]]
+    for task, datasets in TASK_TO_DATASET.items() if task in AVAILABLE_TASK_TO_DATASET
 }
 
 os.makedirs(EXT_DATA_ROOT, exist_ok=True)
--- veceval/veceval_evaluate.py
+++ veceval/veceval_evaluate.py
@@ -2,38 +2,39 @@ import os
 import sys
 from functools import partial
 
-from veceval.settings import CONFIGS_FOLDER, MODES, TASKS, TASK_TO_DATASET
+from veceval.settings import CONFIGS_FOLDER, MODES, TASK_TO_DATASET
 import importlib
 
 
 def compile_trainers():
-    task_to_trainer = {}
-    for task in TASKS:
+    task_to_dataset_to_trainer = {}
+    for task, datasets in TASK_TO_DATASET.items():
         for mode in MODES:
             base_name = f"{task}_{mode}"
             config_file_path = CONFIGS_FOLDER / f"config_{base_name}.txt"
             if not os.path.isfile(config_file_path):
                 continue
-            try:
-                trainer = getattr(importlib.import_module(f"veceval.training.{base_name}"), "main_training")
-                trainer = partial(trainer, config_file_path, TASK_TO_DATASET.get(task))
-            except (ImportError, ModuleNotFoundError, AttributeError):
-                continue
-            task_to_trainer[base_name] = trainer
-    return task_to_trainer
+            dataset_to_trainer = {}
+            for dataset in datasets:
+                try:
+                    trainer = getattr(importlib.import_module(f"veceval.training.{base_name}"), "main_training")
+                    trainer = partial(trainer, config_file_path, dataset)
+                except (ImportError, ModuleNotFoundError, AttributeError):
+                    continue
+                dataset_to_trainer[dataset] = trainer
+            task_to_dataset_to_trainer[base_name] = dataset_to_trainer
+    return task_to_dataset_to_trainer
 
 
 def evaluate_embedding(embedding_name):
     task_to_trainer = compile_trainers()
-    for task_name, trainer in task_to_trainer.items():
-        task_info = task_name.upper()
-        if trainer.args[1]:
-            task_info += "  -  " + trainer.args[1].upper()
-        print(80 * "-")
-        print(task_info)
-        print(80 * "-")
-        trainer(embedding_name)
-        print()
+    for task_name, dataset_to_trainer in task_to_trainer.items():
+        for dataset, trainer in dataset_to_trainer.items():
+            print(80 * "-")
+            print(f"{task_name.upper()} - {dataset.upper()}")
+            print(80 * "-")
+            trainer(embedding_name)
+            print()
 
 
 if __name__ == '__main__':
